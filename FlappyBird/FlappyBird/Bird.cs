fde47117ebd2db7ddc78f1b6651bd5dbf1bb817a
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-03-14 10:08:33 +0100

    Step 2: Draw Sprite and origin

---
+++ FlappyBird/FlappyBird/Bird.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlappyBird
+{
+    class Bird : RotatingSpriteGameObject
+    {
+        public Bird() : base("spr_bird")
+        {
+            //TODO Check if origin is correct
+            origin = new Vector2(sprite.Width, sprite.Height);
+            this.Origin = origin;
+            Vector2 StartScreen = new Vector2(FlappyBird.Screen.X / 2, FlappyBird.Screen.Y / 2);
+
+            this.Position = StartScreen;
+        }
+    }
+}
